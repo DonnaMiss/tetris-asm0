e794ec28d0e64b4e0108649467d8fee5dcd4113b
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2020-06-23 11:35:59 +0000

    New translations default.json (Hebrew)

--- 3rd_party_libs/build_mxnet.sh
+++ 3rd_party_libs/build_mxnet.sh
@@ -1,2 +1,5 @@
 # Download ncnn
-test -e incubator-mxnet || git clone --recursive https://github.com/
+test -e incubator-mxnet || git clone --recursive https://github.com/apache/incubator-mxnet.git
+
+cd incubator-mxnet
+git reset --hard 2fc0706 # 
