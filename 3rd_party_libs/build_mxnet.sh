b3218304a64ececf4806492352dae48f20a3e13c
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2020-06-23 18:29:15 +0000

    language update

--- 3rd_party_libs/build_mxnet.sh
+++ 3rd_party_libs/build_mxnet.sh
@@ -2,4 +2,9 @@
 test -e incubator-mxnet || git clone --recursive https://github.com/apache/incubator-mxnet.git
 
 cd incubator-mxnet
-git reset --hard 2fc0706 # 
+git reset --hard 2fc0706 # 1.8.0.rc3
+git submodule update --init --recursive
+
+mkdir build_amd64
+cd build_amd64
+
