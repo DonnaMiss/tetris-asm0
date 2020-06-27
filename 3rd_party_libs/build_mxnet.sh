fc268f07531f10b3178ef5fe7bc828c5242d92d5
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2020-06-27 07:50:05 +0000

    fix unicode error

--- 3rd_party_libs/build_mxnet.sh
+++ 3rd_party_libs/build_mxnet.sh
@@ -8,4 +8,4 @@ git submodule update --init --recursive
 mkdir build_amd64
 cd build_amd64
 
-cmake -DUSE_CPP_PACKAGE=1 -DBUILD_CPP_EXAMPLES=OFF -DUSE_CUDA=0 -DUSE_
+cmake -DUSE_CPP_PACKAGE=1 -DBUILD_CPP_EXAMPLES=OFF -DUSE_CUDA=0 -DUSE_MKL_IF_AVAILABLE=1 -DUSE_BLAS=mkl -DUSE_OPENCV=0 -DUSE_LAPACK=0 -DUS
