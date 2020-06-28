88d8309e6be6b5687679c71173b88701bb03980f
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2020-06-28 22:04:12 +0000

    fix break

--- 3rd_party_libs/build_mxnet.sh
+++ 3rd_party_libs/build_mxnet.sh
@@ -9,4 +9,9 @@ mkdir build_amd64
 cd build_amd64
 
 cmake -DUSE_CPP_PACKAGE=1 -DBUILD_CPP_EXAMPLES=OFF -DUSE_CUDA=0 -DUSE_MKL_IF_AVAILABLE=1 -DUSE_BLAS=mkl -DUSE_OPENCV=0 -DUSE_LAPACK=0 -DUSE_OPENMP=1 \
- -DMKL_INCLUDE_DIR=/opt/intel/oneapi/mkl/latest/include -DMKL_RT_LIBRARY=/opt/intel/oneapi/mkl/latest/lib/intel64/libmkl_rt.so -DCMAKE_BUILD_TYPE=
+ -DMKL_INCLUDE_DIR=/opt/intel/oneapi/mkl/latest/include -DMKL_RT_LIBRARY=/opt/intel/oneapi/mkl/latest/lib/intel64/libmkl_rt.so -DCMAKE_BUILD_TYPE=Release ..
+
+nproc | xargs -I % make -j%
+
+make DESTDIR=./packaged install
+
