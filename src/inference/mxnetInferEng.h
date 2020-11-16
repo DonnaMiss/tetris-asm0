ea192644d01a89cdc1d43643b1b3d37bedeb994a
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2020-11-16 15:45:56 +0000

    params

--- src/inference/mxnetInferEng.h
+++ src/inference/mxnetInferEng.h
@@ -12,4 +12,6 @@ public:
 private:
     inline NDArray matToNDArray(cv::Mat rgb_image, Context ctx = Context::cpu());
     std::map<std::string, mxnet::cpp::NDArray> m_argsMap;
-    std::map<std::string, mxnet::cpp::N
+    std::map<std::string, mxnet::cpp::NDArray> m_auxMap;
+    mxnet::cpp::Context m_globalCtx;
+    mxnet::cpp::Executo
