bd98f7e770f23a484e92572b0ce78447eb089647
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2020-11-25 07:49:22 +0000

    0.1.43

--- src/inference/ncnnInferEng.cpp
+++ src/inference/ncnnInferEng.cpp
@@ -1,4 +1,6 @@
 #include "ncnnInferEng.h"
 
 InferenceEng::InferenceEng(const std::string &modelDir) {
-    std::cout << "Using ncnn inference engine" <
+    std::cout << "Using ncnn inference engine" << std::endl;
+    std::cout << "-------------------------------------------------" << std::endl;
+    std::co
