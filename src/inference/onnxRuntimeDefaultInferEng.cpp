6b737d141c7a42e1a3ca176b643ef295ee79ee7c
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2021-01-16 10:23:47 +0000

    added initial readme

--- src/inference/onnxRuntimeDefaultInferEng.cpp
+++ src/inference/onnxRuntimeDefaultInferEng.cpp
@@ -41,4 +41,5 @@ void InferenceEng::runInference(const cv::Mat& rgbImage, std::array<float, 500>&
 
     size_t inputTensorSize = 112 * 112 * 3;
 
-    std::vector
+    std::vector<const char*> outputNodeNames = {"fc1"};
+    std::vector<const 
