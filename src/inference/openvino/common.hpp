cfce66d93722dcc7938fbc2ebeae6033c56c2e6a
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2021-08-15 13:39:28 +0000

    Fix filters

--- src/inference/openvino/common.hpp
+++ src/inference/openvino/common.hpp
@@ -273,4 +273,6 @@ static UNUSED void writeOutputBmp(std::vector<std::vector<size_t>> data, size_t 
 
     int padSize = static_cast<int>(4 - (width * 3) % 4) % 4;
     int sizeData = static_cast<int>(width * height * 3 + height * padSize);
-    int sizeAll = si
+    int sizeAll = sizeData + sizeof(file) + sizeof(info);
+
+    file[2] = (unsigned char) (sizeAll);
