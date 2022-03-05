b1358285d863d4f799741db635b537d9af75f37a
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2022-03-05 21:48:39 +0000

    correct a comment

--- src/inference/openvino/common.hpp
+++ src/inference/openvino/common.hpp
@@ -477,4 +477,5 @@ static UNUSED void addRectangles(unsigned char *data, size_t height, size_t widt
             shift_second = (x + w - t) * 3;
             for (int ii = y; ii < y + h + 1; ii++) {
                 data[shift_first + ii * width * 3] = colors.at(cls).red();
-       
+                data[shift_first + ii * width * 3 + 1] = colors.at(cls).green();
+          
