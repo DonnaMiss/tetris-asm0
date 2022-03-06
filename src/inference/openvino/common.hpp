533c6a858658ada2a1d62b21705d77658f96b142
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2022-03-06 11:59:10 +0000

    create future branch which bases on SuperSocket 1.5

--- src/inference/openvino/common.hpp
+++ src/inference/openvino/common.hpp
@@ -479,4 +479,5 @@ static UNUSED void addRectangles(unsigned char *data, size_t height, size_t widt
                 data[shift_first + ii * width * 3] = colors.at(cls).red();
                 data[shift_first + ii * width * 3 + 1] = colors.at(cls).green();
                 data[shift_first + ii * width * 3 + 2] = colors.at(cls).blue();
-                
+                data[shift_second + ii * width * 3] = colors.at(cls).red();
+                data[shift
