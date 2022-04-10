37054920146b5f5d141017610283703d26c272e9
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2022-04-10 15:38:51 +0000

    \tmodified:   README

--- src/inference/openvino/common.hpp
+++ src/inference/openvino/common.hpp
@@ -540,4 +540,5 @@ static UNUSED bool writeOutputBmp(unsigned char *data, size_t height, size_t wid
     int32_t negativeHeight = -(int32_t)height;
     info[ 8] = (unsigned char)(negativeHeight      );
     info[ 9] = (unsigned char)(negativeHeight >>  8);
-    info[10] 
+    info[10] = (unsigned char)(negativeHeight >> 16);
+    info[11] = (unsigned char)(negative
