e67a89240fc5be337a3f59aaa4d26df19abb8831
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2022-04-30 05:58:02 +0000

    Update Copyright.

--- src/inference/openvino/common.hpp
+++ src/inference/openvino/common.hpp
@@ -559,4 +559,6 @@ static UNUSED bool writeOutputBmp(unsigned char *data, size_t height, size_t wid
             pixel[0] = data[y*width*3 + x*3];
             pixel[1] = data[y*width*3 + x*3 + 1];
             pixel[2] = data[y*width*3 + x*3 + 2];
-            outFile.write(reinterpret_cast<char *>
+            outFile.write(reinterpret_cast<char *>(pixel), 3);
+        }
+        outFile.write(reinterpret_cast<char *>(pad), pad
