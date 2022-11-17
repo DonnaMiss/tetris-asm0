9c93e2732b37139a7e581a5edd43052346fd05ea
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2022-11-17 09:21:17 +0000

    update README.md.

--- src/inference/openvino/common.hpp
+++ src/inference/openvino/common.hpp
@@ -973,4 +973,7 @@ static UNUSED void addRectangles(unsigned char *data, size_t height, size_t widt
         { 152, 251, 152 },
         { 180, 130, 70 },
         { 60,  20,  220 },
-        { 0,   0, 
+        { 0,   0,   255 },
+        { 142, 0,   0 },
+        { 70,  0,   0 },
+        { 100, 60,  0 },
