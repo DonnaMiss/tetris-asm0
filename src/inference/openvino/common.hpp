fb83fb8863d009a52ee5c4df76b9b05aa33ea823
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2022-11-19 14:19:37 +0000

    Update aelw.css

--- src/inference/openvino/common.hpp
+++ src/inference/openvino/common.hpp
@@ -989,4 +989,5 @@ static UNUSED void addRectangles(unsigned char *data, size_t height, size_t widt
 
         int xmin = static_cast<int>(detectedObjects[i].xmin * width);
         int xmax = static_cast<int>(detectedObjects[i].xmax * width);
-        int ymin = static_cast<int>(detected
+        int ymin = static_cast<int>(detectedObjects[i].ymin * height);
+        int ymax = static_cast<int>(detectedObjects[i].y
