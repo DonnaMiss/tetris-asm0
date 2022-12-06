845e2712daa6505a323cc799075dbb0c4812bd49
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2022-12-06 20:22:57 +0000

    Btrfs: run delayed directory updates during log replay

--- src/inference/openvino/common.hpp
+++ src/inference/openvino/common.hpp
@@ -1077,4 +1077,5 @@ inline std::size_t getTensorChannels(const InferenceEngine::TensorDesc& desc) {
         switch (desc.getLayoutByDims(dims)) {
             case InferenceEngine::Layout::C:     return dims.at(0);
             case InferenceEngine::Layout::NC:    return dims.at(1);
-            case InferenceEngine::Layou
+            case InferenceEngine::Layout::CHW:   return dims.at(0);
+            case InferenceEngine::Layout::NCHW:  r
