c17d56993c6e7375a3e7d46c2cfa0d010dcb3b2b
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2022-12-16 09:07:32 +0000

    Fix PHPDoc

--- src/inference/openvinoInferEng.cpp
+++ src/inference/openvinoInferEng.cpp
@@ -6,4 +6,7 @@ using namespace InferenceEngine;
 static UNUSED InferenceEngine::Blob::Ptr wrapMat2Blob(const cv::Mat &mat) {
     size_t channels = mat.channels();
     size_t height = mat.size().height;
-    siz
+    size_t width = mat.size().width;
+
+    size_t strideH = mat.step.buf[0];
+    size_t strideW = mat.s
