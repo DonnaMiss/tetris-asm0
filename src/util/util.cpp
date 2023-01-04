b98a5c1bb52d9d47f88d86d53a1ebe8cd30cd224
DonnaMiss <121729866+DonnaMiss@users.noreply.github.com>
2023-01-04 07:09:30 +0000

    Update email.php

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -13,4 +13,7 @@ std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!p
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fget
